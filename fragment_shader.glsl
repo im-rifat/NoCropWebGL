8fcbabea2f05199cb37014f5a48f5a42cc981142
im-rifat <rrahman991@gmail.com>
2024-02-03 17:23:25 +0600

    aspect fit mode implemented.

--- fragment_shader.glsl
+++ fragment_shader.glsl
@@ -1,10 +1,9 @@
-precision mediump float;
+precision highp float;
 
 varying vec2 v_texCoord;
 
 uniform sampler2D u_image;
 uniform vec2 u_scale;
-
 uniform float u_angle;
 uniform float u_inputRatio;
 
@@ -13,8 +12,8 @@ mat2 rotate(float angle) {
 }
 
 void main() {
-    vec2 uv = v_texCoord - vec2(.5);
-    uv /= u_scale;
+    vec2 uv = (v_texCoord - vec2(.5));
+    uv *= u_scale;
 
     uv.x *= u_inputRatio;
     uv *= rotate(u_angle);
@@ -22,8 +21,8 @@ void main() {
 
     uv += vec2(.5);
 
-    if(uv.x >= 0.0 && uv.x <= 1.0 && uv.y >= 0.0 && uv.y <= 1.0) {
-        gl_FragColor = texture2D(u_image, uv);
-    } else
+    if(uv.x < 0. || uv.x > 1. || uv.y < 0. || uv.y > 1.)
         discard;
+
+    gl_FragColor = texture2D(u_image, uv);
 }
--- webgl.js
+++ webgl.js
@@ -26,25 +26,23 @@ function runGL(gl, vertexShaderSource, fragmentShaderSource, image) {
         0, gl.canvas.height,
         gl.canvas.width, 0,
         gl.canvas.width, 0,
-        gl.canvas.width, gl.canvas.height,
-        0, gl.canvas.height
+        0, gl.canvas.height,
+        gl.canvas.width, gl.canvas.height
     ];
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
-
-    console.log(gl.canvas.width + ", " + gl.canvas.height);
 
     const texBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, texBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
-        0, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1
+        0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 1, 1
     ]), gl.STATIC_DRAW);
     const tex = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, tex);
     // Set the parameters so we can render any size image.
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 
     // Upload the image into the texture.
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
@@ -52,14 +50,72 @@ function runGL(gl, vertexShaderSource, fragmentShaderSource, image) {
     var angleInRad = document.getElementById("rotation-range").value;
     var horizontalFlip = -1;
 
-    document.getElementById("rotation-range").addEventListener("input", function() {
+    $("#rotation-range").on("input", function() {
         angleInRad = this.value - 360.0;
         angleInRad = angleInRad*Math.PI / 180.0;
 
         drawScene();
     });
 
+    $("#canvas_4_3").click(function() {
+        scaleToFit(4/3.0);
+        drawScene();
+    });
+
+    $("#canvas_16_9").click(function() {
+
+        scaleToFit(16/9.0);
+        drawScene();
+    });
+
+    $("#canvas_9_16").click(function() {
+
+        scaleToFit(9/16.0);
+        drawScene();
+    });
+
+    $("#canvas_1_1").click(function() {
+
+        scaleToFit(1.0);
+        drawScene();
+    });
+
     drawScene();
+
+    function scaleToFit(ratio) {
+        let canvasHolder = $("#canvas_holder").get(0);
+        let canvas = $("#canvas").get(0);
+
+        if(ratio > 1.0) {
+            canvas.style.width = "100%";
+            let heightPercentage = (100/ratio);
+            canvas.style.height = heightPercentage + "%";
+        } else {
+            canvas.style.height = "100%";
+            let widthPercentage = (canvasHolder.clientHeight*100.0)/canvasHolder.clientWidth * ratio;
+            canvas.style.width = widthPercentage + "%";
+        }
+
+        glutil.needCanvasResize(gl);
+        onCanvasSizeChanged();
+    }
+
+    function onCanvasSizeChanged() {
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+        // 6 2d points
+        const points = [
+            0, 0,
+            0, gl.canvas.height,
+            gl.canvas.width, 0,
+            gl.canvas.width, 0,
+            0, gl.canvas.height,
+            gl.canvas.width, gl.canvas.height
+        ];
+
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points), gl.STATIC_DRAW);
+    }
 
     function drawScene() {
         glutil.needCanvasResize(gl);
@@ -82,35 +138,50 @@ function runGL(gl, vertexShaderSource, fragmentShaderSource, image) {
 
        // var translation = [gl.canvas.width/2, gl.canvas.height/2];
         var scale = [1, 1];
-        const canvasRatio = gl.canvas.width/gl.canvas.height;
-        const imageRatio = image.width / image.height;
+        const frameAspect = gl.canvas.width/gl.canvas.height;
+        const textureAspect = image.width / image.height;
+        const textureFrameRatio = textureAspect/frameAspect;
+        const portraitFrame = frameAspect < 1.0;
+        const portraitTexture = textureAspect < 1.0;
 
-        if(imageRatio/canvasRatio >= 1.0) {
-            scale[0] = 1.;
-            scale[1] = 1.0/imageRatio/canvasRatio;
+            if(frameAspect == 1.0) {
+                if(portraitTexture) {
+                    scale[0] = 1.0/textureFrameRatio;
+                } else {
+                    scale[1] = textureFrameRatio;
+                }
+            }
+
+        else if(portraitTexture) {
+            if(portraitFrame) {
+                scale[1] = textureFrameRatio;
+            } else {
+                scale[0] = 1.0/textureFrameRatio;
+            }
         } else {
-            scale[0] = imageRatio/canvasRatio;
-            scale[1] = 1.;
+            if(portraitFrame) {
+                scale[1] = textureFrameRatio;
+            } else {
+                scale[0] = 1.0/textureFrameRatio;
+            }
         }
 
         gl.uniformMatrix3fv(matrixUniformLocationHandler, false, projectionMatrix);
         gl.uniform2fv(scaleUniform, scale);
         gl.uniform1f(rotationUniform, angleInRad);
-        gl.uniform1f(inputRatioUniform, imageRatio);
+        gl.uniform1f(inputRatioUniform, textureAspect);
         gl.drawArrays(gl.TRIANGLES, 0, 6);
     }
 }
 
 async function main() {
     /** @type {HTMLCanvasElement} */
-    const canvas = document.querySelector("#canvas");
+    const canvas = $("#canvas").get(0);
 
     var gl = canvas.getContext("webgl");
     if (!gl) {
       alert("no webgl for you");
       return;
-    } else {
-      console.log("webgl supported");
     }
 
     var vertexShaderSource = await fetch('vertex_shader.glsl').then(result => result.text());
@@ -119,8 +190,6 @@ async function main() {
     var image = new Image();
     image.src = "moti_mia.jpg";
     image.onload = function() {
-        console.log(image.width + ", " + image.height);
-
         runGL(gl, vertexShaderSource, fragmentShaderSource, image);
     }
     
